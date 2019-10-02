9e5f19fdd70808bf39ee570176cf48d884135c8f
Marcelo <m.cubria@alumnos.urjc.es>
2019-10-02 11:03:45 +0200

    fichero para las pruebas mas legible

--- programaclase.py
+++ programaclase.py
@@ -3,8 +3,19 @@ import socket
 import sys
 
 if __name__== "__main__":
-    a = simplertp.RtpHeader()
-    a.setHeader(2, 0, 0, 0, 0, 90, 1000)
-    b = simplertp.RtpPayloadMp3()
-    b.setAudio('archivo.mp3')
-    simplertp.SendRtpPacket(2, a, b, '127.0.0.1', 33332, 2)
+    cabeceraRTP = simplertp.RtpHeader()
+    version = 2
+    paddingFlag = 0
+    extFlag = 0
+    numeroCSRC = 0
+    marker = 0
+    payloadType = 90
+    ssrc = 1000
+    cabeceraRTP.setHeader(version, paddingFlag, extFlag, numeroCSRC, marker, payloadType, ssrc)
+    audio = simplertp.RtpPayloadMp3()
+    audio.setAudio('archivo.mp3')
+    numeroPaquetesRTP = 2
+    paquetesMP3porRTP = 2
+    ip = '127.0.0.1'
+    port = 33332
+    simplertp.SendRtpPacket(numeroPaquetesRTP, cabeceraRTP, audio, ip, port, paquetesMP3porRTP)
